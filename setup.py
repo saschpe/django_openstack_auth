11eadb9fdcf11746ba9df43aee3e70a9609f83b7
Sascha Peilicke <speilicke@suse.com>
2013-11-25 15:54:22 +0100

    Add tox support
    For that, move dependencies into requirements.txt and
    test-requirements.txt. Those files can be consumed by tox, pip and
    setup.py. Prequisite for testing against several Django versions.

--- setup.py
+++ setup.py
@@ -17,6 +17,41 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
+def parse_requirements(requirements_file='requirements.txt'):
+    requirements = []
+    with open(requirements_file, 'r') as f:
+        for line in f:
+            # For the requirements list, we need to inject only the portion
+            # after egg= so that distutils knows the package it's looking for
+            # such as:
+            # -e git://github.com/openstack/nova/master#egg=nova
+            if re.match(r'\s*-e\s+', line):
+                requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1',
+                                    line))
+            # such as:
+            # http://github.com/openstack/nova/zipball/master#egg=nova
+            elif re.match(r'\s*https?:', line):
+                requirements.append(re.sub(r'\s*https?:.*#egg=(.*)$', r'\1',
+                                    line))
+            # -f lines are for index locations, and don't get used here
+            elif re.match(r'\s*-f\s+', line):
+                pass
+            # -r lines are for including other files, and don't get used here
+            elif re.match(r'\s*-r\s+', line):
+                pass
+            # argparse is part of the standard library starting with 2.7
+            # adding it to the requirements list screws distro installs
+            elif line == 'argparse' and sys.version_info >= (2, 7):
+                pass
+            else:
+                requirements.append(line.strip())
+    return requirements
+
+
+install_requires = parse_requirements("requirements.txt")
+tests_requires = parse_requirements("test-requirements.txt")
+
+
 setup(
     name="django_openstack_auth",
     version=find_version("openstack_auth", "__init__.py"),
@@ -41,12 +76,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
     ],
     zip_safe=False,
-    install_requires=[
-        'django >= 1.4',
-        'python-keystoneclient >= 0.3'
-    ],
-    tests_require=[
-        'mox',
-    ],
+    install_requires=install_requires,
+    tests_require=tests_requires,
     test_suite='openstack_auth.tests.run_tests.run'
 )
